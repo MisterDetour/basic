de8ffc8e80c7742e619dd8cc5492d35c09c750a8
Detour <detour@hybridvigordesign.com>
2014-11-09 20:36:03 -0800

    Fixed script enqueue syntax

--- functions.php
+++ functions.php
@@ -45,8 +45,7 @@ function basic_load_scripts() {
 	wp_enqueue_style( 'reset', get_stylesheet_directory_uri() . '/css/reset.css' );
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-	$url = get_template_directory_uri().'/js/';
-	wp_enqueue_script('custom', $src = $url . 'custom.js', array('jquery'), false, true );
+	wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/js/custom.js', array('jquery'), false, true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
