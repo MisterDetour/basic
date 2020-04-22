fd607db1e1870cb114d6ed7b4272ba5a13ec1b33
Detour <detour@hybridvigordesign.com>
2020-04-22 10:29:14 -0700

    Cleanup nav html

--- header.php
+++ header.php
@@ -23,19 +23,20 @@
 
         <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
             <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
                 <a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>">
                     <?php bloginfo( 'name' ); ?>
                 </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
                 <?php
                 wp_nav_menu( array(
                     'theme_location'    => 'header-menu',
                     'depth'             => 2,
                     'container'         => 'div',
                     'container_class'   => 'collapse navbar-collapse',
-                    'container_id'      => 'bs-example-navbar-collapse-1',
+                    'container_id'      => 'navbar-collapse',
                     'menu_class'        => 'nav navbar-nav',
                     'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                     'walker'            => new WP_Bootstrap_Navwalker(),
