477f1fab7f4a6cde8cad986f74c4fc2478125986
David LaTour <detour@hybridvigordesign.com>
2023-08-25 12:31:39 -0700

    Roll package versions back

--- header.php
+++ header.php
@@ -19,32 +19,32 @@
 
 <div class="wrapper">
 
-    <a class="skip-link screen-reader-text" href="#primary">Skip to content</a>
+	<a class="skip-link screen-reader-text" href="#primary">Skip to content</a>
 
 	<header class="site-header" role="banner">
 
-        <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>">
-                    <?php bloginfo( 'name' ); ?>
-                </a>
-                
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <?php
-                wp_nav_menu( array(
-                    'theme_location'    => 'header-menu',
-                    'depth'             => 2,
-                    'container'         => 'div',
-                    'container_class'   => 'collapse navbar-collapse',
-                    'container_id'      => 'navbar-collapse',
-                    'menu_class'        => 'nav navbar-nav'
-                ) );
-                ?>
-            </div><!-- .container-fluid -->
-        </nav><!-- .navbar -->
+		<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg" alt="<?php bloginfo( 'name' ); ?>">
+				</a>
+				
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				
+				<?php
+				wp_nav_menu( array(
+					'theme_location'    => 'header-menu',
+					'depth'             => 2,
+					'container'         => 'div',
+					'container_class'   => 'collapse navbar-collapse',
+					'container_id'      => 'navbar-collapse',
+					'menu_class'        => 'nav navbar-nav'
+				) );
+				?>
+			</div><!-- .container-fluid -->
+		</nav><!-- .navbar -->
 	</header><!--  .site-header -->
 	
 	<div id="primary" class="main">
