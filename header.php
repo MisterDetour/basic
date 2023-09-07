b3242063522a6f79dc888fe4b78e44f58cd5a912
David LaTour <detour@hybridvigordesign.com>
2023-09-07 09:30:36 -0700

    Fix X-UA meta tag

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 
 <head>
 	
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
